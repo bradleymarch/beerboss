57ca449223803a1dbf35d1af5766fd4571d6bb8c
bradleymarch <bradleymarch@gmail.com>
2017-08-08 10:51:31 -0500

    server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 const runServer = require('./server').runServer;
+const {PORT, DATABASE_URL} = require('./server/config')
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     // Change the cwd to server to mimic running directly
     process.chdir('server');
     // Only require inside the if block so we don't run the server code twice
@@ -11,18 +12,18 @@ if (process.env.NODE_ENV === 'production') {
     const runServer = require('./server').runServer;
     // Just run the server
     runServer(process.env.PORT || 8080);
-}
-else {
-    const app = express();
-    // Proxy everything through to Create React App
-    app.use(proxy('http://localhost:3000/', {
-        logLevel: 'warn', // Keep the logs clean
-        ws: true, // Proxy websockets too
-        router: {
-            // Anything to /api goes to our backend
-            'localhost:8080/api': 'http://localhost:3001'
-        }
-    }));
-    app.listen(process.env.PORT || 9999, () => {
-      console.log(`Your app is listening on port 9999`)});
+// }
+// else {
+//     const app = express();
+//     // Proxy everything through to Create React App
+//     app.use(proxy('http://localhost:3000/', {
+//         logLevel: 'warn', // Keep the logs clean
+//         ws: true, // Proxy websockets too
+//         router: {
+//             // Anything to /api goes to our backend
+//             'localhost:8080/api': 'http://localhost:3001'
+//         }
+//     }));
+    app.listen(process.env.PORT || DATABASE_URL, () => {
+      console.log(`Your app is listening`)});
 }
