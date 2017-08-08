3f4fe83ae91776b7f99914418d67fed79c0d2117
bradleymarch <bradleymarch@gmail.com>
2017-08-08 08:46:47 -0500

    server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {PORT, DATABASE_URL} = require('./server/config');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     // Change the cwd to server to mimic running directly
     process.chdir('server');
     // Only require inside the if block so we don't run the server code twice
@@ -12,17 +12,17 @@ if (process.env.NODE_ENV === 'production') {
     const runServer = require('./server').runServer;
     // Just run the server
     runServer(DATABASE_URL, PORT);
-}
-else {
-    const app = express();
-    // Proxy everything through to Create React App
-    app.use(proxy('http://localhost:3000/', {
-        logLevel: 'warn', // Keep the logs clean
-        ws: true, // Proxy websockets too
-        router: {
-            // Anything to /api goes to our backend
-            'localhost:8080/api': 'http://localhost:3001'
-        }
-    }));
+// }
+// else {
+//     const app = express();
+//     // Proxy everything through to Create React App
+//     app.use(proxy('http://localhost:3000/', {
+//         logLevel: 'warn', // Keep the logs clean
+//         ws: true, // Proxy websockets too
+//         router: {
+//             // Anything to /api goes to our backend
+//             'localhost:8080/api': 'http://localhost:3001'
+//         }
+//     }));
     app.listen(process.env.PORT || 8080);
 }
