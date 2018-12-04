80b90df913965fb1db21a0bd5c7950fd2dc173b6
bradleymarch <bradleymarch@gmail.com>
2018-12-04 15:15:47 -0600

    restructure css

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import LocalBreweries from './components/dashboard/LocalBreweries';
 import SpecificBeer from './components/dashboard/SpecificBeer';
 import Register from './components/Register';
 import './App.css';
-import './Animation.css';
+import '../public/assets/css/Animation.css';
 
 class App extends Component {
 
