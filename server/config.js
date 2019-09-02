988186f36f81ee7523f4954bbbe3e6f283322e42
bradleymarch <bradleymarch@gmail.com>
2019-09-02 13:54:30 -0500

    new mlab db

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	PORT: process.env.PORT || 3001,
-	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://bmarch:bmarch@ds127883.mlab.com:27883/beerboss",
+	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://bmarch1:bmarch1@ds215988.mlab.com:15988/beerbossdb",
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL || "mongodb://bmarch:bmarch@ds149437.mlab.com:49437/beerbosstest",
 };
