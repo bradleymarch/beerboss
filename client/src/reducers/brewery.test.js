6744dc01ae4567a21c0291ccbcaa10670aefca41
Brad March <bradleymarch@gmail.com>
2020-04-27 17:41:25 -0400

    Update brewery.test.js

--- client/src/reducers/brewery.test.js
+++ client/src/reducers/brewery.test.js
@@ -4,7 +4,7 @@ describe('breweryReducer', () => {
   it('Should set the initial state when nothing is passed in', () => {
     const state = breweryReducer(undefined, {});
     expect(state).toEqual({
-      breweries: []
+      breweries: [],
     });
   });
 });
